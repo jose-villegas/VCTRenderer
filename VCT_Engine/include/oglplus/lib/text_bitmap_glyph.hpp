879f42d58a2899af98e5e92cd4e9c5ae4bb1d099
jose-villegas <polonoise@gmail.com>
2015-08-21 00:17:29 -0430

    libs

---
+++ VCT_Engine/include/oglplus/lib/text_bitmap_glyph.hpp
@@ -0,0 +1,28 @@
+/**
+ *  @file oglplus/lib/text_bitmap_glyph.hpp
+ *  @brief All-in-one include file for the separatelly-built bitmap glyph functions
+ *
+ *  @author Matus Chochlik
+ *
+ *  Copyright 2010-2014 Matus Chochlik. Distributed under the Boost
+ *  Software License, Version 1.0. (See accompanying file
+ *  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+#pragma once
+#ifndef OGLPLUS_LIB_TEXT_BITMAP_GLYPH_1208310818_HPP
+#define OGLPLUS_LIB_TEXT_BITMAP_GLYPH_1208310818_HPP
+
+#if !OGLPLUS_NO_VARIADIC_TEMPLATES && GL_VERSION_3_1
+
+#ifndef OGLPLUS_IMPLEMENTING_LIBRARY
+#define OGLPLUS_IMPLEMENTING_LIBRARY 1
+#endif
+
+#include <oglplus/text/bitmap_glyph.hpp>
+
+#undef OGLPLUS_IMPLEMENTING_LIBRARY
+
+#endif // OGLPLUS_NO_VARIADIC_TEMPLATES && GL_VERSION
+
+#endif // include guard
---
+++ VCT_Engine/include/oglplus/lib/text_pango_cairo.hpp
@@ -0,0 +1,30 @@
+/**
+ *  @file oglplus/lib/text_pango_cairo.hpp
+ *  @brief All-in-one include file for the separatelly-built pango/cairo functions
+ *
+ *  @author Matus Chochlik
+ *
+ *  Copyright 2010-2014 Matus Chochlik. Distributed under the Boost
+ *  Software License, Version 1.0. (See accompanying file
+ *  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+#pragma once
+#ifndef OGLPLUS_LIB_TEXT_PANGO_CAIRO_1208310818_HPP
+#define OGLPLUS_LIB_TEXT_PANGO_CAIRO_1208310818_HPP
+
+#if !OGLPLUS_NO_VARIADIC_TEMPLATES
+
+#ifndef OGLPLUS_IMPLEMENTING_LIBRARY
+#define OGLPLUS_IMPLEMENTING_LIBRARY 1
+#endif
+
+#if OGLPLUS_PANGO_CAIRO_FOUND
+#include <oglplus/text/pango_cairo.hpp>
+#endif
+
+#undef OGLPLUS_IMPLEMENTING_LIBRARY
+
+#endif // OGLPLUS_NO_VARIADIC_TEMPLATES
+
+#endif // include guard
