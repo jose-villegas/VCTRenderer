1f7e6b29f04f12206c51d8a833f97fa89fa11c70
José Villegas <polonoise@gmail.com>
2015-10-20 23:25:28 -0700

    Revert "testing turning on planet change, actor status manager"
    This reverts commit dac188360d31890b4940e424641888c6b9c58a30.

--- VCT_Engine/_build/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/Documents/Visual Studio 2015/Projects/VCT_Engine/VCT_Engine/dependencies/assimp/contrib/zlib
-# Build directory: F:/Documents/Visual Studio 2015/Projects/VCT_Engine/VCT_Engine/_build/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
