dac188360d31890b4940e424641888c6b9c58a30
José Villegas <polonoise@gmail.com>
2015-10-20 23:25:01 -0700

    testing turning on planet change, actor status manager

---
+++ VCT_Engine/_build/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: F:/Documents/Visual Studio 2015/Projects/VCT_Engine/VCT_Engine/dependencies/assimp/contrib/zlib
+# Build directory: F:/Documents/Visual Studio 2015/Projects/VCT_Engine/VCT_Engine/_build/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ VCT_Engine/_install64/lib/cmake/assimp-3.1/assimp-config-version.cmake
@@ -0,0 +1,13 @@
+set( PACKAGE_VERSION "3.1.1" )
+if( "${PACKAGE_FIND_VERSION}" VERSION_EQUAL "3.1.1")
+  set(PACKAGE_VERSION_EXACT 1)
+endif()
+if( "${PACKAGE_FIND_VERSION_MAJOR}.${PACKAGE_FIND_VERSION_MINOR}" EQUAL "3" )
+  set(PACKAGE_VERSION_COMPATIBLE 1)
+elseif( "${PACKAGE_FIND_VERSION_MAJOR}" EQUAL "3" )
+  # for now backward compatible if minor version is less
+  if( ${PACKAGE_FIND_VERSION_MINOR}  LESS 1 )
+    set(PACKAGE_VERSION_COMPATIBLE 1)
+  endif()
+endif()
+set( ASSIMP_STATIC_LIB "OFF")
