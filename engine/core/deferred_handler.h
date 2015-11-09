bc32f36f5c85b870001ab5094a78228a548e2fe6
José Villegas <polonoise@gmail.com>
2015-11-09 09:56:21 -0430

    documenting

--- engine/core/deferred_handler.h
+++ engine/core/deferred_handler.h
@@ -34,6 +34,11 @@ class DeferredProgram
         virtual void ExtractUniform(oglplus::SLDataType uType,
                                     std::string uName) = 0;
 
+
+        /// <summary>
+        /// Holds collections of program uniforms of the same type
+        /// these collections have to be identifier by countable identifier
+        /// </summary>
         template<typename T1, typename T2>
         class UniformCollection
         {
@@ -47,7 +52,6 @@ class DeferredProgram
             private:
                 std::vector<std::pair<T2, T1> *> links;
                 std::vector<T2> actives;
-                std::vector<T1> uniforms;
         };
 
     public:
@@ -66,6 +70,10 @@ class DeferredProgram
         void ExtractActiveUniforms();
 };
 
+/// <summary>
+/// Resizes the specified identifier vector
+/// </summary>
+/// <param name="idCount">The identifier count.</param>
 template<typename T1, typename T2>
 void DeferredProgram::UniformCollection<T1, T2>::Resize(const size_t idCount)
 {
@@ -73,22 +81,36 @@ void DeferredProgram::UniformCollection<T1, T2>::Resize(const size_t idCount)
     links.resize(idCount);
 }
 
+/// <summary>
+/// Saves the specified identifier and related program uniform
+/// </summary>
+/// <param name="id">The identifier.</param>
+/// <param name="uniform">The program uniform.</param>
 template<typename T1, typename T2>
 void DeferredProgram::UniformCollection<T1, T2>::Save(T2 id, T1 uniform)
 {
     if (id < 0 && id >= links.size()) { return; }
 
     actives.push_back(std::move(id));
-    uniforms.push_back(std::move(uniform));
-    links[id] = new std::pair<T2, T1>(actives.back(), uniforms.back());;
+    links[id] = new std::pair<T2, T1>(actives.back(), std::move(uniform));;
 }
 
+/// <summary>
+/// Determines whether the class holds an uniform
+/// associated with the specified identifier.
+/// </summary>
+/// <param name="id">The identifier.</param>
+/// <returns>boolean</returns>
 template<typename T1, typename T2>
 bool DeferredProgram::UniformCollection<T1, T2>::Has(const T2 &id) const
 {
     return links[id] != nullptr;
 }
 
+/// <summary>
+/// Active uniforms identifiers
+/// </summary>
+/// <returns>A vector containing all active identifiers</returns>
 template<typename T1, typename T2>
 const std::vector<T2> &DeferredProgram::UniformCollection<T1, T2>::Actives()
 const
@@ -96,6 +118,11 @@ const
     return actives;
 }
 
+/// <summary>
+/// Indexes for the class, returns the id associated uniform
+/// </summary>
+/// <param name="id">The identifier.</param>
+/// <returns>The uniform</returns>
 template<typename T1, typename T2>
 T1 &DeferredProgram::UniformCollection<T1, T2>::operator[](const T2 &id)
 {
